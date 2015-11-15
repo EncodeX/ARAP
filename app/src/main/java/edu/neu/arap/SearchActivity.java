de1ef1664aeb4306f4da57a7419df64c65b8bfa2
志伟 <1083707917@qq.com>
2015-11-15 20:37:18 +0800

    隐藏动画

--- app/src/main/java/edu/neu/arap/SearchActivity.java
+++ app/src/main/java/edu/neu/arap/SearchActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.threed.jpct.Animation;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,7 +18,8 @@ import butterknife.ButterKnife;
 public class SearchActivity extends AppCompatActivity implements MyItemClickListener {
     private Camera camera;
     private Camera.Parameters parameters;
-
+    private TranslateAnimation translateAnimation;
+    private TranslateAnimation translateAnimationUp;
 	@Bind(R.id.findOff)
 	Button findOffButton;
     @Bind(R.id.flashOff)
@@ -42,17 +45,47 @@ public class SearchActivity extends AppCompatActivity implements MyItemClickList
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.addItemDecoration(new SpacesItemDecoration(8));
         mAdapter.setOnItemClickListener(this);
+        translateAnimationUp=new TranslateAnimation(0,0,0,-100);
+        translateAnimationUp.setDuration(618);
     }
     private void clickerListener(){
-        findViewById(R.id.my_recycler_view).setVisibility(View.INVISIBLE);
         findViewById(R.id.textView).setVisibility(View.INVISIBLE);
         findOffButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (findViewById(R.id.findOff).isSelected()) {
                     findViewById(R.id.findOff).setSelected(false);
-                    findViewById(R.id.textView).setVisibility(View.GONE);
-                    findViewById(R.id.my_recycler_view).setVisibility(View.GONE);
+                    translateAnimationUp.setAnimationListener(new android.view.animation.Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(android.view.animation.Animation animation) {
+                        }
+
+                        @Override
+                        public void onAnimationEnd(android.view.animation.Animation animation) {
+                            findViewById(R.id.textView).setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(android.view.animation.Animation animation) {
+                        }
+                    });
+                    findViewById(R.id.textView).startAnimation(translateAnimationUp);
+                    translateAnimation=new TranslateAnimation(0,0,0,findViewById(R.id.recycler_view_canvas).getHeight());
+                    translateAnimation.setDuration(618);
+                    translateAnimation.setAnimationListener(new android.view.animation.Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(android.view.animation.Animation animation) {
+                        }
+                        @Override
+                        public void onAnimationEnd(android.view.animation.Animation animation) {
+                            findViewById(R.id.my_recycler_view).setVisibility(View.GONE);
+                        }
+                        @Override
+                        public void onAnimationRepeat(android.view.animation.Animation animation) {
+                        }
+                    });
+                    findViewById(R.id.my_recycler_view).startAnimation(translateAnimation);
+                    view.startAnimation(translateAnimation);
                 } else {
                     findViewById(R.id.findOff).setSelected(true);
                     findViewById(R.id.textView).setVisibility(View.VISIBLE);
