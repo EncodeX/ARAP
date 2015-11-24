7ea174a2c41e80ae65098f60bbcbe07c95a5dc7f
志伟 <1083707917@qq.com>
2015-11-24 22:25:09 +0800

    重写部分动画

--- app/src/main/java/edu/neu/arap/SearchActivity.java
+++ app/src/main/java/edu/neu/arap/SearchActivity.java
@@ -6,14 +6,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,22 +21,19 @@ import butterknife.ButterKnife;
 public class SearchActivity extends AppCompatActivity implements MyItemClickListener {
     private Camera camera;
     private Camera.Parameters parameters;
-    private float recyclerViewHeight;
-    private TranslateAnimation translateAnimationRecyclerView;
-    private TranslateAnimation translateAnimationTextViewUp;
-    private TranslateAnimation translateAnimationRecyclerViewUp;
-    private RelativeLayout.LayoutParams layoutParams;
-    private RelativeLayout.LayoutParams layoutParams2;
     private String[] spinnerData={"全部","餐饮","交通","学习","住宿","娱乐","购物"};
     private boolean flag=true;
 
-	@Bind(R.id.explore_button)
-	Button findOffButton;
+    @Bind(R.id.explore_button)
+    Button exploreButton;
     @Bind(R.id.flashlight_button)
-    Button flashOffButton;
+    Button flashButton;
     @Bind(R.id.filter_spinner)
     Spinner spinner;
-
+    @Bind(R.id.explore_list)
+    RecyclerView exploreList;
+    @Bind(R.id.main_menu_button)
+    Button menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,8 +54,6 @@ public class SearchActivity extends AppCompatActivity implements MyItemClickList
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.addItemDecoration(new SpacesItemDecoration(8));
         mAdapter.setOnItemClickListener(this);
-        translateAnimationTextViewUp =new TranslateAnimation(0,0,0,-100);
-        translateAnimationTextViewUp.setDuration(618);
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, spinnerData));
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -74,28 +69,29 @@ public class SearchActivity extends AppCompatActivity implements MyItemClickList
     }
     private void clickerListener(){
         findViewById(R.id.hint_text).setVisibility(View.INVISIBLE);
-        findOffButton.setOnClickListener(new View.OnClickListener() {
+        exploreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (findViewById(R.id.explore_button).isSelected()) {
                     findViewById(R.id.explore_button).setSelected(false);
-                    findViewById(R.id.hint_text).startAnimation(translateAnimationTextViewUp);
-                    findViewById(R.id.explore_list).startAnimation(translateAnimationRecyclerView);
-                    view.startAnimation(translateAnimationRecyclerView);
                 } else {
-                    findOffButton.setSelected(true);
+                    exploreButton.setSelected(true);
                     findViewById(R.id.hint_text).setVisibility(View.VISIBLE);
-                    if(flag)
-                    {
-                        setLayout();
-                    }
-                    findViewById(R.id.explore_list).startAnimation(translateAnimationRecyclerViewUp);
-                    view.startAnimation(translateAnimationRecyclerViewUp);
-                    findOffButton.setLayoutParams(layoutParams2);
+                    ObjectAnimator exploreListUp=ObjectAnimator.ofFloat(exploreList,"translationY",0,-exploreList.getHeight());
+                    exploreListUp.setDuration(600);
+                    ObjectAnimator exploreButtonUp=ObjectAnimator.ofFloat(exploreButton,"translationY",0,-exploreList.getHeight());
+                    exploreButtonUp.setDuration(600);
+                    ObjectAnimator menuUp=ObjectAnimator.ofFloat(menu,"translationY",0,-exploreList.getHeight());
+                    menuUp.setDuration(600);
+                    ObjectAnimator spinnerUp=ObjectAnimator.ofFloat(spinner,"translationY",0,-exploreList.getHeight());
+                    spinnerUp.setDuration(600);
+                    AnimatorSet exploreUp=new AnimatorSet();
+                    exploreUp.play(exploreButtonUp).with(exploreListUp).with(menuUp).with(spinnerUp);
+                    exploreUp.start();
                 }
             }
         });
-        flashOffButton.setOnClickListener(new View.OnClickListener() {
+        flashButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (findViewById(R.id.flashlight_button).isSelected()) {
@@ -116,66 +112,6 @@ public class SearchActivity extends AppCompatActivity implements MyItemClickList
                     });
                     camera.startPreview();
                 }
-            }
-        });
-    }
-
-    private void setLayout(){
-        flag=false;
-        recyclerViewHeight=findViewById(R.id.expand_area).getHeight();
-        layoutParams=new RelativeLayout.LayoutParams(findOffButton.getHeight(),findOffButton.getWidth());
-        layoutParams2=new RelativeLayout.LayoutParams(findOffButton.getHeight(),findOffButton.getWidth());
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        layoutParams2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        layoutParams2.addRule(RelativeLayout.ABOVE, R.id.expand_area);
-        layoutParams.rightMargin=8;
-        layoutParams2.rightMargin=8;
-        translateAnimationRecyclerViewUp=new TranslateAnimation(0,0,recyclerViewHeight,0);
-        translateAnimationRecyclerViewUp.setDuration(618);
-        translateAnimationRecyclerViewUp.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                findViewById(R.id.explore_list).setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-        });
-        translateAnimationTextViewUp.setAnimationListener(new android.view.animation.Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(android.view.animation.Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(android.view.animation.Animation animation) {
-                findViewById(R.id.hint_text).setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationRepeat(android.view.animation.Animation animation) {
-            }
-        });
-        translateAnimationRecyclerView =new TranslateAnimation(0,0,0,recyclerViewHeight);
-        translateAnimationRecyclerView.setDuration(618);
-        translateAnimationRecyclerView.setAnimationListener(new android.view.animation.Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(android.view.animation.Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(android.view.animation.Animation animation) {
-                findViewById(R.id.explore_list).setVisibility(View.INVISIBLE);
-                findOffButton.setLayoutParams(layoutParams);
-            }
-
-            @Override
-            public void onAnimationRepeat(android.view.animation.Animation animation) {
             }
         });
     }
